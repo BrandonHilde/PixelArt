293ca13716aae6972244843d4af2653c180c92c2
Brandon Hilde <bjames.12299@gmail.com>
2018-04-11 09:21:10 -0700

    Pixel Art

---
+++ pixelart/pixelart/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace pixelart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(1217, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add Color";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(1217, 70);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(120, 516);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "#FFFFFF\n#C0C0C0\n#808080\n#000000\n#FF0000\n#800000\n#FFFF00\n#808000\n#00FF00\n#008000\n#" +
+    "00FFFF\n#008080\n#0000FF\n#000080\n#FF00FF\n#800080";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(591, 574);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(609, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(602, 574);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 634);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 43);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Open File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 608);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(151, 20);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 589);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Spacing";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(259, 634);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(151, 43);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Convert";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(578, 634);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(151, 43);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Save";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.Location = new System.Drawing.Point(1217, 41);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(120, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Sample Colors";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1349, 689);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ pixelart/pixelart/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pixelart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Bitmap btm = new Bitmap(1, 1);
+        Bitmap bBt = new Bitmap(1, 1);
+
+        Color[] clrs = new Color[1];
+        Graphics g = null;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ColorDialog cd = new ColorDialog();
+                cd.ShowDialog();
+
+                richTextBox1.Text += ColorTranslator.ToHtml(cd.Color);
+                richTextBox1.Text += "\r\n";
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+
+                ofd.ShowDialog();
+
+                btm = new Bitmap(ofd.FileName);
+
+
+                pictureBox1.Image = btm;
+            }
+            catch
+            {
+
+            }
+        }
+
+        public Color Clr(Color[] cs)
+        {
+            Color c = Color.Black;
+
+            int r = 0;
+            int g = 0;
+            int b = 0;
+
+            for(int i = 0; i < cs.Length; i++)
+            {
+                r += cs[i].R;
+                g += cs[i].G;
+                b += cs[i].B;
+            }
+
+            r /= cs.Length;
+            g /= cs.Length;
+            b /= cs.Length;           
+
+            int near = 1000;
+            int ind = 0;
+
+            for(int cl = 0; cl < clrs.Length; cl++)
+            {
+                int valR = (clrs[cl].R - r);
+                int valG = (clrs[cl].G - g);
+                int valB = (clrs[cl].B - b);
+
+                if (valR < 0) valR = -valR;
+                if (valG < 0) valG = -valG;
+                if (valB < 0) valB = -valB;
+
+                int total = valR + valG + valB;
+
+                if (total < near)
+                {
+                    ind = cl;
+                    near = total;
+                }
+            }
+
+            c = clrs[ind];
+
+            return c;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            clrs = new Color[richTextBox1.Lines.Length];
+
+            for (int v = 0; v < richTextBox1.Lines.Length; v++)
+            {
+                try
+                {
+                    clrs[v] = ColorTranslator.FromHtml(richTextBox1.Lines[v]);
+                }
+                catch
+                {
+                    clrs[v] = Color.Transparent;
+                }
+            }
+
+            int num = (int)numericUpDown1.Value;
+
+            btm = (Bitmap)pictureBox1.Image;
+            bBt = new Bitmap(btm.Width, btm.Height);
+
+            using (g = Graphics.FromImage(bBt))
+            {
+                List<Color> block = new List<Color>();
+
+                Rectangle rec = new Rectangle();
+
+                SolidBrush sb = new SolidBrush(Color.Black);
+
+                Color final = Color.Black;
+
+                for (int x = 0; x < btm.Width; x += num)
+                {
+                    for (int y = 0; y < btm.Height; y += num)
+                    {
+                        block = new List<Color>();
+
+                        for (int v = 0; v < num; v++)
+                        {
+                            for (int c = 0; c < num; c++)
+                            {
+                                if (x + v < btm.Width && y + c < btm.Height)
+                                {
+                                    block.Add(btm.GetPixel(x + v, y + c));
+                                }
+                            }
+                        }
+
+                        if (block.Count > 0)
+                        {
+                            final = Clr(block.ToArray());
+
+                            sb.Color = final;
+
+                            rec.X = x;
+                            rec.Y = y;
+                            rec.Width = num;
+                            rec.Height = num;
+
+                            g.FillRectangle(sb, rec);
+                        }
+                    }
+                }
+
+                pictureBox2.Image = bBt;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Png Files|*.png";
+            sfd.ShowDialog();
+
+            bBt.Save(sfd.FileName);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (btm != null)
+            {
+                if (btm.Width > 1)
+                {
+                    richTextBox1.Text = "#FFFFFF\r\n#000000\r\n";
+
+                    Random r = new Random(DateTime.Now.Millisecond);
+
+                    for(int i = 0; i < 16; i++)
+                    {
+                        int x = r.Next(0, btm.Width);
+                        int y = r.Next(0, btm.Height);
+                        richTextBox1.Text += ColorTranslator.ToHtml(btm.GetPixel(x, y));
+                        richTextBox1.Text += "\r\n";
+                    }
+                }
+            }
+        }
+    }
+}
